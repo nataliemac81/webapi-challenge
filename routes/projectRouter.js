8e9cb65d58ecae38fec701a1d0f8f24d78ac07c5
Natalie McCroy <nataliemccroy@gmail.com>
2019-08-16 21:47:29 -0500

    put and delete endpoints working

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -62,15 +62,62 @@ router.get('/:id/actions', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-
+	const project = req.body
+	const name = project.name
+	const description = project.description
+	if (!name || !description) {
+		res.status(400).json({
+			message: "Please provide name and description for the project."
+		})
+	}
+	projectDb.insert(project)
+		.then(project => {
+			res.status(201).json(project)
+		})
+		.catch(err => {
+			res.status(500).json({
+				err: err,
+				message: 'There was an error while saving the project to the database'
+			})
+		})
 })
 
-router.put('/', (req, res) => {
-
+router.put('/:id', (req, res) => {
+	const { id } = req.params
+	const changes = req.body
+	
+	projectDb.get(id).then(project => {
+		if(project) {
+			projectDb.update(id, changes).then(updated => {
+				if (!changes.name || !changes.description) {
+					res.status(400).json({
+						message: "Please provide a name and description for the post."
+					})
+				} else {
+					res.status(200).json(updated)
+				}
+			})
+		}
+	})
 })
 
-router.delete('/', (req, res) => {
+router.delete('/:id', (req, res) => {
+	const { id } = req.params
 
+	projectDb.remove(id)
+	.then(project => {
+		if (project) {
+			res.status(200).json({ message: "Project deleted."})
+		} else {
+			res.status(404).json({ message: "A project with that ID does not exist."})
+		}	
+	})
+	.catch(err => {
+		res.status(500).json({
+			err: err,
+			message: "The project could not be removed"
+		})
+	})
 })
 
 
