b1793b1bd0fc0534fafc0ea792213f7d342e1d3d
Natalie McCroy <nataliemccroy@gmail.com>
2019-08-16 20:18:27 -0500

    /GET endpoints working

---
+++ routes/actionRouter.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+const projectDb = require('../data/helpers/projectModel')
+const actionDb = require('../data/helpers/actionModel')
+
+
+
+
+module.exports = router
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -5,3 +5,51 @@ const router = express.Router()
 const projectDb = require('../data/helpers/projectModel')
 const actionDb = require('../data/helpers/actionModel')
 
+router.get('/', (req, res) => {
+	projectDb.get()
+	.then(projects => {
+		res.status(200).json(projects)
+	})
+	.catch(err => {
+		res.status(500).json({
+			err: err,
+			message: "Could not retrieve projects"
+		})
+	})
+})
+
+router.get('/:id', (req, res) => {
+	const { id } = req.params
+	
+	projectDb.get(id)
+	.then(project => {
+		if(project) {
+			res.status(200).json(project)
+		} else {
+			res.status(404).json({
+				message: "The project with the specified ID does not exist."
+			})
+		}
+	})
+	.catch(err => {
+		res.status(500).json({
+			err: err,
+			message: "The project information could not be retrieved."
+		})
+	})
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.put('/', (req, res) => {
+
+})
+
+router.delete('/', (req, res) => {
+
+})
+
+
+module.exports = router
